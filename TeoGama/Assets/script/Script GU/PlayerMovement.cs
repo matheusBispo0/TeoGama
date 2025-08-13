0526515b07e2609323ae28391818a2c66406a2e1
GustavoPereiraAabreu <gustavop.4breu@gmail.com>
2025-08-13 16:26:15 -0300

    Enemy shooter completo

--- TeoGama/Assets/script/Script GU/PlayerMovement.cs
+++ TeoGama/Assets/script/Script GU/PlayerMovement.cs
@@ -1,49 +1,27 @@
 using UnityEngine;
 
-
-
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerMovement : MonoBehaviour
-
 {
-
     public float moveSpeed = 5f;
-
     public float jumpForce = 10f;
-
     public Rigidbody2D rb;
-
     public Vector2 moveInput;
 
-
-
     public void Awake()
-
     {
-
         rb = GetComponent<Rigidbody2D>();
-
     }
 
-
-
     public void Update()
-
     {
-
         float moveX = Input.GetAxisRaw("Horizontal");
-
         moveInput = new Vector2(moveX, 0f).normalized;
 
-
-
         if (Input.GetKeyDown(KeyCode.Space))
-
         {
-
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-
         }
 
     }
@@ -51,11 +29,8 @@ public class PlayerMovement : MonoBehaviour
 
 
     public void FixedUpdate()
-
     {
-
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
-
     }
 
 }
