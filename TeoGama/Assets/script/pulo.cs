c4061490a4b8a1cb92461bd6f72e73ae26a97968
GustavoPereiraAabreu <gustavop.4breu@gmail.com>
2025-08-13 15:31:58 -0300

    Arrumando erros

--- TeoGama/Assets/script/pulo.cs
+++ TeoGama/Assets/script/pulo.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 
 
 public class pulo : MonoBehaviour
-
 {
 
-    // Define a velocidade do jogador. "public" permite que você a edite no editor do Unity.
+  // Define a velocidade do jogador. "public" permite que vocï¿½ a edite no editor do Unity.
 
-    public float speed = 5f;
+  public float speed = 5f;
 
 
 
@@ -19,24 +18,22 @@ public class pulo : MonoBehaviour
 
 
     void Start()
-
     {
 
-        // Pega o componente Rigidbody2D no início do jogo.
+     // Pega o componente Rigidbody2D no inï¿½cio do jogo.
 
-        rb = GetComponent<Rigidbody2D>();
+     rb = GetComponent<Rigidbody2D>();
 
     }
 
 
 
     void Update()
-
     {
 
-        // Lê a entrada horizontal do teclado (teclas 'A'/'D' ou setas).
+     // Lï¿½ a entrada horizontal do teclado (teclas 'A'/'D' ou setas).
 
-        float horizontalInput = Input.GetAxis("Horizontal");
+      float horizontalInput = Input.GetAxis("Horizontal");
 
 
 
@@ -48,7 +45,7 @@ public class pulo : MonoBehaviour
 
         // Define a velocidade do Rigidbody2D.
 
-        rb.velocity = movement * speed;
+        rb.linearVelocity = movement * speed;
 
     }
 
