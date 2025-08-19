fa169a082cb571223a6f57f62e489619014eb47f
matheusBispo0 <matheus.bispo@alunos.sc.senac.br>
2025-08-19 16:34:16 -0300

    quadro

--- TeoGama/Assets/script/scripts matheus/MovimentoPlayer.cs
+++ TeoGama/Assets/script/scripts matheus/MovimentoPlayer.cs
@@ -1,34 +1,51 @@
 using UnityEngine;
 
 public class MovimentoPlayer : MonoBehaviour
+
 {
+
     public float moveSpeed = 5f;
+
     public float jumpForce = 7f;
+
     private Rigidbody2D rb;
+
     private bool isGrounded;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    [SerializeField] public Transform groundCheck;
+
+    [SerializeField] private float checkRadius = 0.2f;
+
     [SerializeField] private LayerMask groundLayer;
+
     void Start()
+
     {
+
         rb = GetComponent<Rigidbody2D>();
+
     }
+
     void Update()
+
     {
+
         float moveInput = Input.GetAxisRaw("Horizontal");
+
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && isGrounded)
+
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+
         {
+
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
         }
+
     }
-    void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    }
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-    }
+
 }
---
+++ TeoGama/Assets/script/scripts matheus/Quadro.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+public class Quadro : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private HingeJoint2D hinge;
+    private bool caiu = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Static; 
+                                              
+        hinge = gameObject.AddComponent<HingeJoint2D>();
+        hinge.enabled = false;
+    }
+    void OnMouseDown()
+    {
+        if (!caiu)
+        {
+            caiu = true;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            hinge.enabled = true;
+            hinge.anchor = new Vector2(-0.5f, 0.5f);
+            hinge.connectedAnchor = hinge.anchor;
+            hinge.connectedBody = null;
+        }
+    }
+}
