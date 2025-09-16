e6638b1ee460a8d4c9ffbb0c33879e68b4801252
GustavoPereiraAabreu <gustavop.4breu@gmail.com>
2025-09-16 15:39:06 -0300

    SistemaDePause

---
+++ TeoGama/Assets/script/Script GU/QuitGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+    public void QuitMenu()
+    {
+        SceneManager.LoadScene("iniciar (Melissa)");
+    }
+
+}
+ 
