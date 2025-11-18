2d77bb5a77cafd5d0ff322d864b6a1cd78ad9bb6
matheusBispo0 <matheus.bispo@alunos.sc.senac.br>
2025-11-18 11:11:38 -0300

    ada

--- TeoGama/Assets/script/Scripts_Matheus/Teleporte.cs
+++ TeoGama/Assets/script/Scripts_Matheus/Teleporte.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class Teleporte : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public GameObject interactUI;
     public string nomeDaCena; // Nome da cena que deseja carregar
     private bool jogadorPerto = false;
@@ -12,6 +13,11 @@ public class Teleporte : MonoBehaviour
     {
         interactUI.SetActive(false);
     }
+=======
+    public string nomeDaCena; // Nome da cena para onde vai teleportar
+    private bool jogadorPerto = false;
+
+>>>>>>> Stashed changes
     void Update()
     {
         if (jogadorPerto && Input.GetKeyDown(KeyCode.E))
@@ -20,23 +26,40 @@ public class Teleporte : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
     private void OnTriggerEnter2D(Collider2D other)
+=======
+    void OnTriggerEnter2D(Collider2D other)
+>>>>>>> Stashed changes
     {
         if (other.CompareTag("Player"))
         {
             jogadorPerto = true;
+<<<<<<< Updated upstream
             interactUI.SetActive(true);
 
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
+=======
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+>>>>>>> Stashed changes
     {
         if (other.CompareTag("Player"))
         {
             jogadorPerto = false;
+<<<<<<< Updated upstream
             interactUI.SetActive(false);
             
         }
     }
 }
+=======
+        }
+    }
+}
+>>>>>>> Stashed changes
